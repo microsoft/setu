0f54dd659d5de0776717c9269e99536fe053ebe4
Roshan Dathathri <roshan.dathathri@microsoft.com>
2025-09-25 16:09:34 -0700

    Update license in the file header

--- csrc/test/native/SampleNativeTest.cpp
+++ csrc/test/native/SampleNativeTest.cpp
@@ -1,6 +1,6 @@
 //==============================================================================
-// Copyright 2025 Vajra Team; Georgia Institute of Technology; Microsoft
-// Corporation
+// Copyright (c) 2025 Vajra Team; Georgia Institute of Technology; Microsoft
+// Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
