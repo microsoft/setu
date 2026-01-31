560eb9839ad60ea70bd8a01be9ce1a45f8e5002b
Elton Leander Pinto <eltonp3103@gmail.com>
2026-01-31 00:23:43 -0500

    Add copyright comment

--- csrc/setu/planner/Planner.h
+++ csrc/setu/planner/Planner.h
@@ -1,14 +1,31 @@
-
+//==============================================================================
+// Copyright (c) 2025 Vajra Team; Georgia Institute of Technology; Microsoft
+// Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//==============================================================================
 #pragma once
-
+//==============================================================================
 #include "commons/StdCommon.h"
 #include "commons/Types.h"
+//==============================================================================
 #include "commons/datatypes/CopySpec.h"
 #include "commons/datatypes/Device.h"
 #include "ir/Instruction.h"
 #include "metastore/MetaStore.h"
-
+//==============================================================================
 namespace setu::planner {
+//==============================================================================
 
 using setu::commons::NodeId;
 using setu::commons::datatypes::CopySpec;
@@ -39,4 +56,6 @@ class Planner {
   virtual ~Planner() = default;
   virtual Plan Compile(CopySpec& spec, MetaStore& metastore) = 0;
 };
+//==============================================================================
 }  // namespace setu::planner
+//==============================================================================
