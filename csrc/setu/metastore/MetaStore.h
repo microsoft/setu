635abc7592ee590cfb1e0cf22be1e837ed558633
Elton Leander Pinto <eltonp3103@gmail.com>
2026-01-31 00:29:50 -0500

    Empty out MetaStore implementation

--- csrc/setu/metastore/MetaStore.h
+++ csrc/setu/metastore/MetaStore.h
@@ -16,93 +16,18 @@
 //==============================================================================
 #pragma once
 //==============================================================================
-#include "commons/ClassTraits.h"
-#include "commons/Logging.h"
 #include "commons/StdCommon.h"
-#include "commons/Types.h"
-//==============================================================================
-#include "commons/datatypes/TensorShardRef.h"
-#include "commons/datatypes/TensorShardSpec.h"
 //==============================================================================
 namespace setu::metastore {
-//==============================================================================
-using setu::commons::ShardId;
-using setu::commons::TensorName;
-using setu::commons::datatypes::TensorShardRef;
-using setu::commons::datatypes::TensorShardSpec;
 //==============================================================================
 /**
  * @brief Metadata store for managing tensor shard registrations
  *
- * MetaStore is responsible for tracking all registered tensor shards in the
- * system. It assigns unique identifiers to shards and provides lookup
- * capabilities for retrieving shard metadata.
+ * TODO: Implement MetaStore functionality
  */
 class MetaStore {
  public:
   MetaStore() = default;
-
-  /**
-   * @brief Registers a new tensor shard in the metadata store
-   *
-   * Creates a new shard registration from the provided specification. A unique
-   * shard ID is generated and assigned to the shard. The shard metadata is
-   * stored for future lookups.
-   *
-   * @param shard_spec The specification describing the tensor shard to register
-   * @return TensorShardRef containing the assigned shard ID and metadata
-   */
-  [[nodiscard]] TensorShardRef RegisterTensorShard(
-      const TensorShardSpec& shard_spec /*[in]*/);
-
-  /**
-   * @brief Looks up a tensor shard by its unique identifier
-   *
-   * @param shard_id The UUID of the shard to look up
-   * @return Optional containing the TensorShardRef if found, empty otherwise
-   */
-  [[nodiscard]] std::optional<TensorShardRef> GetShardById(
-      const ShardId& shard_id /*[in]*/) const;
-
-  /**
-   * @brief Gets all shards registered for a given tensor name
-   *
-   * @param tensor_name The name of the tensor to query
-   * @return Vector of TensorShardRef for all shards of the specified tensor
-   */
-  [[nodiscard]] std::vector<TensorShardRef> GetShardsByTensorName(
-      const TensorName& tensor_name /*[in]*/) const;
-
-  /**
-   * @brief Returns the total number of registered shards
-   *
-   * @return Number of shards in the metadata store
-   */
-  [[nodiscard]] std::size_t GetNumShards() const;
-
-  [[nodiscard]] bool AllShardsRegistered(const TensorName& tensor_name) const;
-
-  /**
-   * @brief Returns the number of shards registered for a given tensor
-   *
-   * @param tensor_name The name of the tensor to query
-   * @return Number of shards registered for this tensor
-   */
-  [[nodiscard]] std::size_t GetNumShardsForTensor(
-      const TensorName& tensor_name /*[in]*/) const;
-
- private:
-  /// Map from shard ID to shard reference
-  std::unordered_map<ShardId, TensorShardRef> shards_by_id_;
-
-  /// Map from tensor name to list of shard IDs for that tensor
-  std::unordered_map<TensorName, std::vector<ShardId>> shards_by_tensor_name_;
-
-  /// Map from tensor name to expected total number of elements
-  std::unordered_map<TensorName, std::size_t> tensor_expected_size_;
-
-  /// Map from tensor name to currently registered number of elements
-  std::unordered_map<TensorName, std::size_t> tensor_registered_size_;
 };
 //==============================================================================
 }  // namespace setu::metastore
