84387895a7ca2c527e30d3308a4b570919c7e6ce
Bhumika Chopra <bhumikasunilchopra@gmail.com>
2026-02-09 11:10:06 -0500

    Add setu cluster start script (assumes we have an existing ray cluster)

--- setu/ray/__init__.py
+++ setu/ray/__init__.py
@@ -5,10 +5,12 @@ Provides Ray-based orchestration for Coordinator and NodeAgent processes
 across a pre-existing Ray cluster.
 """
 
+from setu.ray.cli import main as start_cluster_cli
 from setu.ray.cluster import ClusterInfo, NodeAgentInfo, SetuCluster
 
 __all__ = [
     "SetuCluster",
     "ClusterInfo",
     "NodeAgentInfo",
+    "start_cluster_cli",
 ]
