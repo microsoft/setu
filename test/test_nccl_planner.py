dd4eaca8c1ff3ccb69ed5e272715327ee2621f06
Bhumika Chopra <bhumikasunilchopra@gmail.com>
2026-02-17 20:40:57 -0500

    Restore test_nccl_planner.py

--- test/test_nccl_planner.py
+++ test/test_nccl_planner.py
@@ -6,8 +6,8 @@ import uuid
 
 import torch
 
-from setu._planner import MetaStore, NCCLPlanner
-from setu._commons.datatypes import (
+from setu._native import MetaStore, NCCLPlanner
+from setu._native.datatypes import (
     CopySpec,
     Device,
     TensorDim,
